d1383dfa96c5e7225c23f82349b912b10fb8f723
Hemapriya S T <46585387+Hemapriya98@users.noreply.github.com>
2019-06-18 00:20:07 +0530

    Create p15.py

---
+++ p15.py
@@ -0,0 +1,9 @@
+nin=int(input())
+lin=list(map(int,input().split()))
+m1=[]
+for i in lin:
+	s=bin(i)[2::]
+	m1.append(([s.count("1"),i]))
+m1.sort(reverse=True)
+for i in range(0,len(m1)):
+	print(m1[i][1])
