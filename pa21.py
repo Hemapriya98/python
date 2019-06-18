33852544268f2ae5e7212dc88f928c9c4d349010
Hemapriya S T <46585387+Hemapriya98@users.noreply.github.com>
2019-06-18 11:25:53 +0530

    Create pa21.py

---
+++ pa21.py
@@ -0,0 +1,12 @@
+
+nin,k=map(int,input().split(" "))
+a,b=map(int,input().split(" "))
+c,d=map(int,input().split(" "))
+if nin==a==c:
+	print("yes")
+elif k==b==d:
+	print("yes")
+elif n1==k and a==b and c==d:
+	print("yes")
+else:
+	print("no")
