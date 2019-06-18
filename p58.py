97ce6837e8718d2533eab9af5e4a0827bcca8366
Hemapriya S T <46585387+Hemapriya98@users.noreply.github.com>
2019-06-18 19:14:35 +0530

    Create p58.py

---
+++ p58.py
@@ -0,0 +1,14 @@
+number = int(input())
+b = []
+ain = number//2 + number
+for i in range(1,number+1):
+  if i%2==0:
+    b.append(i)
+for i in range(1,number+1):
+  if i%2!=0:
+    b.append(i)
+for i in range(1,number+1):
+  if i%2==0:
+    b.append(i)
+print(ain)
+print(*b)
