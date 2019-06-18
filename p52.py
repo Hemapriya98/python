b3711724efde69b3436123532a94b90d8b682f2b
Hemapriya S T <46585387+Hemapriya98@users.noreply.github.com>
2019-06-18 15:34:10 +0530

    Create p52.py

---
+++ p52.py
@@ -0,0 +1,12 @@
+ain,bin=list(map(int,input().split()))
+cin,din=list(map(int,input().split()))
+ein,fin=list(map(int,input().split()))
+gin,hin=list(map(int,input().split()))
+m=d-b
+n=f-h
+o=e-c
+p=g-a
+if (m==n==o==p):
+    print("yes")
+else:
+    print("no")
