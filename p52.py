42e7c83207c36bb561e0959eb00f0b4df9d1659e
Hemapriya S T <46585387+Hemapriya98@users.noreply.github.com>
2019-06-18 15:36:11 +0530

    Update p52.py

--- p52.py
+++ p52.py
@@ -1,11 +1,11 @@
-ain,bin=list(map(int,input().split()))
+ain,bi=list(map(int,input().split()))
 cin,din=list(map(int,input().split()))
 ein,fin=list(map(int,input().split()))
 gin,hin=list(map(int,input().split()))
-m=d-b
-n=f-h
-o=e-c
-p=g-a
+m=din-bi
+n=fin-hin
+o=ein-cin
+p=gin-ain
 if (m==n==o==p):
     print("yes")
 else:
